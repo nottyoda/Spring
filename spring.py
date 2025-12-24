6b14e41cdbfa941ddd5089d03fb45c934e06bf9d
LIM YONG CHENG ALAN <211384U@mymail.nyp.edu.sg>
2025-12-24 22:42:58 +0800

    window stuff uptill entry

--- spring.py
+++ spring.py
@@ -2,3 +2,47 @@ import numpy as np
 from matplotlib import pyplot as plt
 from tkinter import *
 
+
+#the code using symplectic euler
+#def run():
+    #variables
+    #ls = 1 #length of spring
+    #rp = 1 #radius of mass
+    #mass = 1 #mass of point mass
+
+    #functions
+
+    #def force(theta):
+
+
+    #def alpha(theta, force, mass):
+
+
+#window stuff
+window = Tk()
+window.geometry("200x200")
+window.title("Bouncepadset")
+icon = PhotoImage(file='astro_card.png')
+window.iconphoto(True, icon)
+
+#stuff in the window
+label = Label(window,
+              text="CATCH!!",
+              font=('Impact',50,'bold'),
+              relief=RAISED,
+              bd=10,
+              padx= 10,
+              pady=20)
+label.pack()
+
+entry = Entry(window, 
+              font=("Arial", 50),
+              )
+entry.pack
+run_button = Button(window, text= "Run", command= run)
+run_button.pack()
+
+
+
+window.mainloop()
+
