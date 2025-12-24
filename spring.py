f542d6acf1c76ecd4beff61adb445c9f07afc9a8
LIM YONG CHENG ALAN <211384U@mymail.nyp.edu.sg>
2025-12-24 23:05:59 +0800

    labelled entries in the window

--- spring.py
+++ spring.py
@@ -4,18 +4,28 @@ from tkinter import *
 
 
 #the code using symplectic euler
-#def run():
+def run():
     #variables
-    #ls = 1 #length of spring
-    #rp = 1 #radius of mass
-    #mass = 1 #mass of point mass
+    ls = float(lengthspring.get()) #length of spring
+    rm = float(radiusmass.get()) #radius of mass movement
+    mass = float(massmass.get()) #mass of point mass
 
     #functions
-
+    
     #def force(theta):
 
 
     #def alpha(theta, force, mass):
+
+
+    #checking variables
+    lengthspring.delete(0,END)
+    radiusmass.delete(0,END)
+    massmass.delete(0,END)
+
+    lengthspring.insert(0,ls)
+    radiusmass.insert(0,rm)
+    massmass.insert(0,mass)
 
 
 #window stuff
@@ -33,13 +43,48 @@ label = Label(window,
               bd=10,
               padx= 10,
               pady=20)
-label.pack()
 
-entry = Entry(window, 
+
+label_1 = Label(window,
+                text="Length of spring:",
+                font=("Impact",10)
+                )
+label_1.pack()
+
+lengthspring = Entry(window, 
               font=("Arial", 50),
               )
-entry.pack
-run_button = Button(window, text= "Run", command= run)
+lengthspring.pack()
+
+
+
+label_2 = Label(window,
+                text="Radius of Path:",
+                font=("Impact",10)
+                )
+label_2.pack()
+
+radiusmass = Entry(window, 
+              font=("Arial", 50),
+              )
+radiusmass.pack()
+
+label_3 = Label(window,
+                text="Mass of mass:",
+                font=("Impact",10)
+                )
+label_3.pack()
+
+massmass = Entry(window,
+                 font=("arial", 50),
+                 )
+massmass.pack()
+
+
+run_button = Button(window, 
+                    text= "Run", 
+                    font=("Arial", 50), 
+                    command= run)
 run_button.pack()
 
 
